2d8f5f7c3c5669cbdfcabf60846170c3dddbeb2d
lutovinova_mariya <bukovka09@gmail.com>
2026-04-06 13:38:27 +0300

    feat: add sort_by_date function for sorting by date

--- src/processing.py
+++ src/processing.py
@@ -3,5 +3,13 @@ def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
     return [item for item in data if item.get('state') == state]
 
 
+def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Сортирует список словарей по ключу 'date'.
 
+    :param data: Список словарей для сортировки.
+    :param reverse: Порядок сортировки (True — убывание, False — возрастание).
+    :return: Новый отсортированный список.
+    """
+    return sorted(data, key=lambda x: x.get('date', ''), reverse=reverse)
 
