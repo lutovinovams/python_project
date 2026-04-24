ca4d124d4e91da3756a46b508cf436038e418ed2
lutovinova_mariya <bukovka09@gmail.com>
2026-04-24 10:45:42 +0300

    изменить логику маскировки карт и счетов

--- src/masks.py
+++ src/masks.py
@@ -1,25 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
-    """Функция маскировки банковской карты"""
-    """Преобразуем в строку на случай, если передано число"""
-    card_str = str(card_number)
+    """Маскирует номер карты, если он корректной длины."""
+    if len(card_number) != 16 or not card_number.isdigit():
+        return "Invalid card number"
 
-    """Берем первые 6 и последние 4 цифры"""
-    first_part = card_str[:6]
-    last_part = card_str[-4:]
-
-    """Формируем маску: 6 цифр + 6 звездочек (вместо скрытых) + 4 цифры"""
-    masked: str = f"{first_part[:4]} {first_part[4:]}** **** {last_part}"
-
-    return masked
-
-
-print(get_mask_card_number("7000792289606361"))
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
-    """Функция маскировки номера банковского счета"""
-    """Берем срез последних 4 символов и добавляем две звездочки в начале"""
+    """Маскирует номер счета, если он корректной длины."""
+    if len(account_number) < 4 or not account_number.isdigit():
+        return "Invalid account number"
+
     return f"**{account_number[-4:]}"
-
-
-print(get_mask_account("73654108430135874305"))
